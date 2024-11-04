b5feaa849595562d39d30089af007a04e88c80ab
ydiv6013 <yogesh.dhameliya@outlook.com>
2024-11-04 14:35:07 +0000

    booscaper project that scraps the data from actual websites html page

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -4,9 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy import Field
 
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    upc = scrapy.Field()
+    image_url = scrapy.Field()
+    url = scrapy.Field()
     pass
